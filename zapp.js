0badc14904c909ef713063d6aeded84185d707ff
Will Ridgers <will@mindfuzz.net>
2013-09-06 12:48:49 +0100

    Added ability to change port number

--- zapp.js
+++ zapp.js
@@ -2,21 +2,29 @@
 
 "use strict";
 
-// libs
-var express  = require('express');
-var chokidar = require('chokidar');
-var sockjs   = require('sockjs');
-var mime     = require('mime');
-
+// node modules
 var http     = require('http');
 var fs       = require('fs');
+
+// libs
+var chokidar = require('chokidar');
+var express  = require('express');
+var mime     = require('mime');
+var sockjs   = require('sockjs');
+
+// arguments
+var argv     = require('optimist')
+                .usage('Usage: $0 -p [port]')
+                .default('p', 8080)
+                .argv;
+
 
 // payloads
 var headerPayload = '<script src="http://cdn.sockjs.org/sockjs-0.3.min.js"></script>';
 var footerPayload = '<script>var sockjs=new SockJS("/socket");sockjs.onmessage=function(e){location.reload();};</script>';
 
 // config
-var port = 80;
+var port = argv.p;
 var serv = process.cwd();
 
 // setup watcher
@@ -80,12 +88,15 @@ function middleware(req, res, next) {
             var index = path.lastIndexOf('.');
             var ext   = (index < 0) ? '' : path.substr(index);          
 
+            // it'd be nice if we could compile ejs/jade/stylus/sass/markdown here
+
             // inject our scripts
             if (ext == '.html') {
               data = data.replace('<\/head>', headerPayload + '\n</head>');
               data = data.replace('<\/body>', footerPayload + '\n</body>');
             }
 
+            // serve the file with correct mime type
             res.set('Content-Type', mime.lookup(path));
             res.send(data);
           }
@@ -101,5 +112,5 @@ function middleware(req, res, next) {
 app.use(middleware);
 
 // start server
-server.listen(80);
+server.listen(port);
 console.log('listening on port ' + port)
